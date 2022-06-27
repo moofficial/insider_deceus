c1d874751ce5b17105557195baaa957aab64932a
Amodups <62596200+Amodups@users.noreply.github.com>
2022-06-27 17:39:25 +0100

    URL update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ describe('Login', () => {
 
 
 it('user is should login with a valid email and password', function(){
-  cy.visit('https://m.n11.com/giris-yap/', {failOnStatusCode: false})
+  cy.visit('https://www.n11.com/giris-yap/', {failOnStatusCode: false})
   cy.get('#email').type('pp@gmail.com')
   cy.get('#password').type('Test12')
   cy.get('#showPassword').click()
--- cypress/e2e/loginwithfacebook.cy.js
+++ cypress/e2e/loginwithfacebook.cy.js
@@ -3,7 +3,7 @@ describe('Login with facebook', () => {
 
     
     it('user should be redirected to facebook and login with valid facebook credentials', function(){
-      cy.visit('https://m.n11.com/giris-yap/')
+      cy.visit('https://www.n11.com/giris-yap/')
       cy.get('#loginForm > .facebook').click() 
       cy.get('[data-testid="royal_email"]').type('pietro55@xbpantibody.com')
       cy.get('#passContainer').type('Test')
